c9e3cb4664554849430d16effc87f34681d4333a
Axel PASCON <axlpascon@gmail.com>
2024-09-30 22:48:20 +0200

    Add some paging API

--- zerOS/include/kernel/memory/paging.h
+++ zerOS/include/kernel/memory/paging.h
@@ -18,9 +18,96 @@ enum zerOS_page_privilege_level
     zerOS_PAGE_DRIVER1_PRIVILEGE,
     zerOS_PAGE_DRIVER2_PRIVILEGE,
     zerOS_PAGE_USER_PRIVILEGE,
-    zerOS_PAGE_CONTAINERS_PRIVILEGE // Only when 5-level paging is enabled
+    zerOS_PAGE_CONTAINERS_PRIVILEGE
 };
 
-#include <kernel/memory/paging_structures.h>
+enum zerOS_page_table_type
+{
+    zerOS_PAGE_TABLE_PML4 = 0,
+    zerOS_PAGE_TABLE_PDP,
+    zerOS_PAGE_TABLE_PD,
+    zerOS_PAGE_TABLE_PT
+};
+
+enum zerOS_page_translation_size
+{
+    zerOS_PAGE_TRANSLATION_4K = 0,
+    zerOS_PAGE_TRANSLATION_2M,
+    zerOS_PAGE_TRANSLATION_1G
+};
+
+enum zerOS_page_table_bits
+    UNDERLYING_TYPE(uint64_t)
+{
+    zerOS_PAGE_PRESENT_BIT  = UINT64_C(1) << 0,
+    zerOS_PAGE_RW_BIT       = UINT64_C(1) << 1,
+    zerOS_PAGE_US_BIT       = UINT64_C(1) << 2,
+    zerOS_PAGE_PWT_BIT      = UINT64_C(1) << 3,
+    zerOS_PAGE_PCD_BIT      = UINT64_C(1) << 4,
+    zerOS_PAGE_ACCESSED_BIT = UINT64_C(1) << 5,
+    zerOS_PAGE_DIRTY_BIT    = UINT64_C(1) << 6,
+    zerOS_PAGE_PS_BIT       = UINT64_C(1) << 7,
+    zerOS_PAGE_GLOBAL_BIT   = UINT64_C(1) << 8,
+    zerOS_PAGE_NX_BIT       = UINT64_C(1) << 63,
+
+    zerOS_PAGE_PAT_PS0_BIT  = UINT64_C(1) << 7,  // When in Page Table Entry (i.e. PS bit is 0)
+    zerOS_PAGE_PAT_PS1_BIT  = UINT64_C(1) << 12  // When in Page Directory Entry, or
+                                                 // in Page Directory Pointer Entry (i.e. PS bit is 1)
+};
+
+/*
+ * NOTE: `zerOS_PAGE_FLAG_<flag>` flags below are only an API, and not an actual representation
+ * of a page table memory layout, hence, the values they are shifted to are not necessarily
+ * meaningful.
+ */
+
+#undef  zerOS_PAGE_FLAG_PRESENT
+#undef  zerOS_PAGE_FLAG_RW
+#undef  zerOS_PAGE_FLAG_US
+#undef  zerOS_PAGE_FLAG_PWT
+#undef  zerOS_PAGE_FLAG_PCD
+#undef  zerOS_PAGE_FLAG_ACCESSED
+#undef  zerOS_PAGE_FLAG_DIRTY
+#undef  zerOS_PAGE_FLAG_PS
+#undef  zerOS_PAGE_FLAG_GLOBAL
+#undef  zerOS_PAGE_FLAG_NX
+#undef  zerOS_PAGE_FLAG_PKE
+
+#undef  zerOS_PAGE_FLAG_CUSTOM
+
+#define zerOS_PAGE_FLAG_PRESENT  (       UINT64_C( 1 ) << 0 )
+#define zerOS_PAGE_FLAG_RW       (       UINT64_C( 1 ) << 1 )
+#define zerOS_PAGE_FLAG_US       (       UINT64_C( 1 ) << 2 )
+#define zerOS_PAGE_FLAG_PWT      (       UINT64_C( 1 ) << 3 )
+#define zerOS_PAGE_FLAG_PCD      (       UINT64_C( 1 ) << 4 )
+#define zerOS_PAGE_FLAG_ACCESSED (       UINT64_C( 1 ) << 5 )
+#define zerOS_PAGE_FLAG_DIRTY    (       UINT64_C( 1 ) << 6 )
+#define zerOS_PAGE_FLAG_PS       (       UINT64_C( 1 ) << 7 )
+#define zerOS_PAGE_FLAG_GLOBAL   (       UINT64_C( 1 ) << 8 )
+#define zerOS_PAGE_FLAG_NX       (       UINT64_C( 1 ) << 9 )
+#define zerOS_PAGE_FLAG_PKE(val) (__PGFLAG_PKE_MK(val) << 10)
+
+#define zerOS_PAGE_FLAG_CUSTOM(val) __PGFLAG_AVL_MK(val)
+
+#undef  __PGFLAG_PKE_MK
+#define __PGFLAG_PKE_MK(v) ((uint64_t)v & UINT64_C(0b1111))
+
+#undef  __PGFLAG_AVL_MK
+#define __PGFLAG_AVL_MK(v) ((uint64_t)v & UINT64_C(0x3ff))
+
+#undef  zerOS_mk_page_struct
+/**
+ * @def zerOS_mk_page_struct()
+ * @todo Document (and finish) this
+ */
+#define zerOS_mk_page_struct(           \
+    buf, memcpyfunc, translationsize,   \
+    addr, pagetype, classicflags,       \
+    customflags, pat                    \
+)                                       \
+({                                      \
+    /* TODO: Finish this */             \
+})
+
 
 #endif
--- zerOS/include/kernel/memory/paging_structures.h
+++ zerOS/include/kernel/memory/paging_structures.h
@@ -17,17 +17,20 @@ struct TYPE_PACKED zerOS_pml4_entry
     BITFIELD_VALUE(write_through, 1);
     BITFIELD_VALUE(cache_disabled, 1);
     BITFIELD_VALUE(accessed, 1);
-    BITFIELD_VALUE(_ignored1, 1);
-    BITFIELD_VALUE(_mbz, 2);
-    BITFIELD_VALUE(avail, 3);
+    BITFIELD_VALUE(_reserved, 3); // Must be 0
+    BITFIELD_VALUE(_avail1, 3); // Available for use by the OS
     BITFIELD_VALUE(pdp_base_address, 40);
-    BITFIELD_VALUE(_ignored2, 11);
+    BITFIELD_VALUE(_avail2, 11); // Available for use by the OS
     BITFIELD_VALUE(execute_disable, 1);
 };
 
 static_assert(
     sizeof(struct zerOS_pml4_entry) * 8 == 64,
     "struct zerOS_pml4_entry is not 64 bits wide"
+);
+static_assert(
+    1 + 1 + 1 + 1 + 1 + 1 + 3 + 3 + 40 + 11 + 1 == 64,
+    "struct zerOS_pml4_entry bitfield values do not add up to 64"
 );
 
 struct TYPE_PACKED zerOS_pdp_entry
@@ -38,11 +41,11 @@ struct TYPE_PACKED zerOS_pdp_entry
     BITFIELD_VALUE(write_through, 1);
     BITFIELD_VALUE(cache_disabled, 1);
     BITFIELD_VALUE(accessed, 1);
-    BITFIELD_VALUE(_ignored1, 1);
-    BITFIELD_VALUE(_mbz, 1);
-    BITFIELD_VALUE(avail, 3);
+    BITFIELD_VALUE(_reserved, 1); // Must be 0
+    BITFIELD_VALUE(large_page, 1);
+    BITFIELD_VALUE(_avail1, 3); // Available for use by the OS
     BITFIELD_VALUE(pd_base_address, 40);
-    BITFIELD_VALUE(_ignored2, 11);
+    BITFIELD_VALUE(_avail2, 11); // Available for use by the OS
     BITFIELD_VALUE(execute_disable, 1);
 };
 
@@ -50,5 +53,32 @@ static_assert(
     sizeof(struct zerOS_pdp_entry) * 8 == 64,
     "struct zerOS_pdp_entry is not 64 bits wide"
 );
+static_assert(
+    1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 3 + 40 + 11 + 1 == 64,
+    "struct zerOS_pdp_entry bitfield values do not add up to 64"
+);
+
+struct TYPE_PACKED zerOS_pd_entry
+{
+    BITFIELD_VALUE(present, 1);
+    BITFIELD_VALUE(read_write, 1);
+    BITFIELD_VALUE(user_supervisor, 1);
+    BITFIELD_VALUE(write_through, 1);
+    BITFIELD_VALUE(cache_disabled, 1);
+    BITFIELD_VALUE(accessed, 1);
+    BITFIELD_VALUE(dirty, 1);
+    BITFIELD_VALUE(_reserved, 1); // Must be 0
+    BITFIELD_VALUE(global, 1);
+    BITFIELD_VALUE(_avail1, 3); // Available for use by the OS
+    BITFIELD_VALUE(pt_base_address, 40);
+    BITFIELD_VALUE(_avail2, 11); // Available for use by the OS
+    BITFIELD_VALUE(execute_disable, 1);
+};
+
+static_assert(
+    sizeof(struct zerOS_pd_entry) * 8 == 64,
+    "struct zerOS_pd_entry is not 64 bits wide"
+);
+
 
 #endif
