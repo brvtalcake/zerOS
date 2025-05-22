8abe913495ca22d76a4a99f3200fddce64ec5186
Axel PASCON <axlpascon@gmail.com>
2025-05-22 02:09:02 +0200

    various utility macros - a custom `proc_macro` to generate variant name strings with all cases at once - `const_eval_select`, directly stolen from rust's core library (private `core::intrinsics::const_eval_select!`), which facilitates the use of the eponymous intrinsic function

--- macro-utils/src/lib.rs
+++ macro-utils/src/lib.rs
@@ -1,6 +1,93 @@
 #![feature(concat_idents)]
+#![feature(decl_macro)]
+
 #![no_std]
 #![recursion_limit = "1024"]
+
+/// Stolen from `core::intrinsics::const_eval_select!`.
+///
+/// A macro to make it easier to invoke `const_eval_select`. Use as follows:
+/// ```rust,ignore (just a macro example)
+/// const_eval_select!(
+///     @capture { arg1: i32 = some_expr, arg2: T = other_expr } -> U:
+///     if const #[attributes_for_const_arm] {
+///         // Compile-time code goes here.
+///     } else #[attributes_for_runtime_arm] {
+///         // Run-time code goes here.
+///     }
+/// )
+/// ```
+/// The `@capture` block declares which surrounding variables / expressions can be
+/// used inside the `if const`.
+/// Note that the two arms of this `if` really each become their own function, which is why the
+/// macro supports setting attributes for those functions. The runtime function is always
+/// markes as `#[inline]`.
+///
+/// See [`const_eval_select()`] for the rules and requirements around that intrinsic.
+pub macro const_eval_select {
+    (
+        @capture$([$($binders:tt)*])? { $($arg:ident : $ty:ty = $val:expr),* $(,)? } $( -> $ret:ty )? :
+        if const
+            $(#[$compiletime_attr:meta])* $compiletime:block
+        else
+            $(#[$runtime_attr:meta])* $runtime:block
+    ) => {
+        // Use the `noinline` arm, after adding explicit `inline` attributes
+        $crate::const_eval_select!(
+            @capture$([$($binders)*])? { $($arg : $ty = $val),* } $(-> $ret)? :
+            #[noinline]
+            if const
+                #[inline] // prevent codegen on this function
+                $(#[$compiletime_attr])*
+                $compiletime
+            else
+                #[inline] // avoid the overhead of an extra fn call
+                $(#[$runtime_attr])*
+                $runtime
+        )
+    },
+    // With a leading #[noinline], we don't add inline attributes
+    (
+        @capture$([$($binders:tt)*])? { $($arg:ident : $ty:ty = $val:expr),* $(,)? } $( -> $ret:ty )? :
+        #[noinline]
+        if const
+            $(#[$compiletime_attr:meta])* $compiletime:block
+        else
+            $(#[$runtime_attr:meta])* $runtime:block
+    ) => {{
+        $(#[$runtime_attr])*
+        fn runtime$(<$($binders)*>)?($($arg: $ty),*) $( -> $ret )? {
+            $runtime
+        }
+
+        $(#[$compiletime_attr])*
+        const fn compiletime$(<$($binders)*>)?($($arg: $ty),*) $( -> $ret )? {
+            // Don't warn if one of the arguments is unused.
+            $(let _ = $arg;)*
+
+            $compiletime
+        }
+
+        ::core::intrinsics::const_eval_select(($($val,)*), compiletime, runtime)
+    }},
+    // We support leaving away the `val` expressions for *all* arguments
+    // (but not for *some* arguments, that's too tricky).
+    (
+        @capture$([$($binders:tt)*])? { $($arg:ident : $ty:ty),* $(,)? } $( -> $ret:ty )? :
+        if const
+            $(#[$compiletime_attr:meta])* $compiletime:block
+        else
+            $(#[$runtime_attr:meta])* $runtime:block
+    ) => {
+        $crate::const_eval_select!(
+            @capture$([$($binders)*])? { $($arg : $ty = $arg),* } $(-> $ret)? :
+            if const
+                $(#[$compiletime_attr])* $compiletime
+            else
+                $(#[$runtime_attr])* $runtime
+        )
+    },
+}
 
 #[macro_export]
 macro_rules! callback {
@@ -110,6 +197,39 @@ macro_rules! max {
             else
             { max!(@inner $(,$rest)*) }
          } };
+}
+
+#[repr(usize)]
+pub enum CaseKind
+{
+	LowerCase = 0,
+	UpperCase,
+	UpperCamelCase,
+	LowerCamelCase,
+	SnakeCase,
+	KebabCase,
+	ShoutySnakeCase,
+	TitleCase,
+	ShoutyKebabCase,
+	TrainCase
+}
+
+pub struct MultiCaseStaticString
+{
+	strings: [&'static str; 10]
+}
+
+impl MultiCaseStaticString
+{
+	pub const fn new(strings: [&'static str; 10]) -> Self
+	{
+		Self { strings }
+	}
+
+	pub const fn get(&self, style: CaseKind) -> &'static str
+	{
+		self.strings[style as usize]
+	}
 }
 
 #[cfg(test)]
--- proc-macro-utils/src/lib.rs
+++ proc-macro-utils/src/lib.rs
@@ -44,16 +44,29 @@ use std::{
 	str::FromStr
 };
 
+use heck::{
+	ToKebabCase,
+	ToLowerCamelCase,
+	ToShoutyKebabCase,
+	ToShoutySnakeCase,
+	ToSnakeCase,
+	ToTitleCase,
+	ToTrainCase,
+	ToUpperCamelCase
+};
 use num_traits::FromPrimitive;
 use proc_macro::TokenStream as TokenStreamClassic;
 use proc_macro2::{Delimiter, Group, Span, TokenStream, TokenTree};
 use quote::{ToTokens, format_ident, quote};
 use syn::{
 	Attribute,
+	DeriveInput,
 	Expr,
 	ExprAssign,
+	Fields,
 	Generics,
 	Ident,
+	ItemEnum,
 	Lit,
 	LitStr,
 	StaticMutability,
@@ -2297,6 +2310,131 @@ pub fn runnable(input: TokenStreamClassic, annotated_item: TokenStreamClassic)
 	let expansion = item.execute_code(&args);
 
 	quote! { #decl_start = #expansion; }.into()
+}
+
+#[proc_macro_attribute]
+pub fn gen_variant_names(
+	_input: TokenStreamClassic,
+	annotated_item: TokenStreamClassic
+) -> TokenStreamClassic
+{
+	let item = parse_macro_input!(annotated_item as ItemEnum);
+
+	fn mk_lower(string: String) -> String
+	{
+		string.to_lowercase()
+	}
+	fn mk_upper(string: String) -> String
+	{
+		string.to_uppercase()
+	}
+	fn mk_upper_camel(string: String) -> String
+	{
+		string.to_upper_camel_case()
+	}
+	fn mk_lower_camel(string: String) -> String
+	{
+		string.to_lower_camel_case()
+	}
+	fn mk_snake(string: String) -> String
+	{
+		string.to_snake_case()
+	}
+	fn mk_kebab(string: String) -> String
+	{
+		string.to_kebab_case()
+	}
+	fn mk_shouty_snake(string: String) -> String
+	{
+		string.to_shouty_snake_case()
+	}
+	fn mk_title(string: String) -> String
+	{
+		string.to_title_case()
+	}
+	fn mk_shouty_kebab(string: String) -> String
+	{
+		string.to_shouty_kebab_case()
+	}
+	fn mk_train(string: String) -> String
+	{
+		string.to_train_case()
+	}
+
+	let styled_string_constructors = [
+		mk_lower as fn(String) -> String,
+		mk_upper as fn(String) -> String,
+		mk_upper_camel as fn(String) -> String,
+		mk_lower_camel as fn(String) -> String,
+		mk_snake as fn(String) -> String,
+		mk_kebab as fn(String) -> String,
+		mk_shouty_snake as fn(String) -> String,
+		mk_title as fn(String) -> String,
+		mk_shouty_kebab as fn(String) -> String,
+		mk_train as fn(String) -> String
+	];
+	let mut variant_names_list = vec![];
+	let mut variant_matchers_list = vec![];
+	for variant in item.variants.clone()
+	{
+		let variant_ident = variant.ident.clone();
+		let variant_name = variant.ident.clone().to_string();
+		match variant.fields
+		{
+			Fields::Named(..) =>
+			{
+				variant_matchers_list.push(quote! {
+					Self::#variant_ident { .. }
+				})
+			},
+			Fields::Unnamed(..) =>
+			{
+				variant_matchers_list.push(quote! {
+					Self::#variant_ident ( .. )
+				})
+			},
+			Fields::Unit =>
+			{
+				variant_matchers_list.push(quote! {
+					Self::#variant_ident
+				})
+			},
+		}
+		let mut styled_strings = vec![];
+		for get_styled in styled_string_constructors
+		{
+			styled_strings.push(get_styled(variant_name.clone()));
+		}
+		variant_names_list.push(quote! {
+			<MultiCaseStaticString>::new([ #(#styled_strings,)* ])
+		});
+	}
+
+	let enum_name = item.ident.clone();
+	let (impl_generics, ty_generics, where_clause) = item.generics.split_for_impl();
+
+	let variant_count = item.variants.len();
+
+	let variant_index = 0..variant_count;
+
+	let returned_impl = quote! {
+		impl #impl_generics #enum_name #ty_generics #where_clause
+		{
+			const VARIANT_NAMES_GENERATED: [MultiCaseStaticString; #variant_count] = [
+				#(#variant_names_list,)*
+			];
+
+			pub const fn variant_name(&self, style: CaseKind) -> &'static str
+			{
+				match self
+				{
+					#(#variant_matchers_list => Self::VARIANT_NAMES_GENERATED[#variant_index].get(style),)*
+					_ => unreachable!()
+				}
+			}
+		}
+	};
+	quote! { #item #returned_impl }.into()
 }
 
 //#[proc_macro_attribute]
